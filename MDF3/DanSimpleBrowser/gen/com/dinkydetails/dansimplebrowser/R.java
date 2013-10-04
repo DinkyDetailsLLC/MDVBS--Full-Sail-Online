af6f371db7aa98373a9b2b1492c679ee2e31a11d
Dan <Dan@Dinkydetails.com>
2013-10-03 21:23:52 -0400

    Setting up my .xml files and strings in the resources files

--- MDF3/DanSimpleBrowser/gen/com/dinkydetails/dansimplebrowser/R.java
+++ MDF3/DanSimpleBrowser/gen/com/dinkydetails/dansimplebrowser/R.java
@@ -24,18 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int goButton=0x7f080002;
+        public static final int menu_settings=0x7f080004;
+        public static final int urlContainer=0x7f080000;
+        public static final int urlField=0x7f080001;
+        public static final int webView=0x7f080003;
     }
     public static final class layout {
         public static final int activity_web_view=0x7f030000;
     }
     public static final class menu {
-        public static final int web_view=0x7f070000;
+        public static final int activity_web_view=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int btn=0x7f050005;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_web_view=0x7f050003;
+        public static final int urlhint=0x7f050004;
     }
     public static final class style {
         /** 
