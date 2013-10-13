3ae77e091e29685fb4920b84dd068cb5c12b03e1
Dan <Dan@Dinkydetails.com>
2013-10-12 23:44:30 -0400

    Adding a Site history activity

--- MDF3/DanSimpleBrowser/gen/com/dinkydetails/dansimplebrowser/R.java
+++ MDF3/DanSimpleBrowser/gen/com/dinkydetails/dansimplebrowser/R.java
@@ -25,13 +25,17 @@ public final class R {
     }
     public static final class id {
         public static final int goButton=0x7f080002;
-        public static final int menu_settings=0x7f080004;
+        public static final int lv_name=0x7f080004;
+        public static final int menu_settings=0x7f080006;
+        public static final int textViewName=0x7f080005;
         public static final int urlContainer=0x7f080000;
         public static final int urlField=0x7f080001;
         public static final int webView=0x7f080003;
     }
     public static final class layout {
         public static final int activity_web_view=0x7f030000;
+        public static final int activitylistview=0x7f030001;
+        public static final int urllayout=0x7f030002;
     }
     public static final class menu {
         public static final int activity_web_view=0x7f070000;
@@ -43,6 +47,7 @@ public final class R {
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_web_view=0x7f050003;
         public static final int urlhint=0x7f050004;
+        public static final int visitedurls=0x7f050006;
     }
     public static final class style {
         /** 
