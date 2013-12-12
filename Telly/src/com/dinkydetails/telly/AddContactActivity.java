f289d44e32b39eb26588440c2b5e2e70768dc978
Dan <Dan@Dinkydetails.com>
2013-12-11 22:03:40 -0500

    Went back to just my XMLs and my  Main Activity. Starting from Scratch

--- Telly/src/com/dinkydetails/telly/AddContactActivity.java
+++ Telly/src/com/dinkydetails/telly/AddContactActivity.java
@@ -1,24 +1,3 @@
 package com.dinkydetails.telly;
 
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
-
-/** New Activity that will run when the app is launched.  */
-/** Need to move the Main Activity to a different location... but still have it prompt, maybe...  */
-public class AddContactActivity  extends Activity{
-
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.add_contact);
-		
-		
-		
-	}
-	
-}
