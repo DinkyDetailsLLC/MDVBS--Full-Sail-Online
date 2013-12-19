b3da9f284bbfa3d4a8f126087533f61c146977a0
Dan <Dan@Dinkydetails.com>
2013-12-19 15:53:34 -0500

    Cannot get info to pass to second activity and launch second screen

---
+++ MovieCheck/src/com/dinkydetails/moviecheck/third.java
@@ -0,0 +1,13 @@
+package com.dinkydetails.moviecheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class third extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.third);
+		TextView name=(TextView)findViewById(R.id.name);
+		TextView date=(TextView)findViewById(R.id.date);
+}}
