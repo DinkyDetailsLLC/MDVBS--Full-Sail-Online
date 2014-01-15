97c84348e573e4f492f62effb222069b3f315a6d
Dan <Dan@Dinkydetails.com>
2014-01-14 21:48:14 -0500

    Added Some XML Files and starting to work on string names and such

--- ADP2/BabyWalk/gen/com/dinkydetails/babywalk/R.java
+++ ADP2/BabyWalk/gen/com/dinkydetails/babywalk/R.java
@@ -8,7 +8,18 @@
 package com.dinkydetails.babywalk;
 
 public final class R {
+    public static final class array {
+        public static final int date=0x7f070000;
+        public static final int month=0x7f070001;
+        public static final int year=0x7f070002;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int grey_font=0x7f040001;
+        public static final int light_font=0x7f040000;
+        public static final int text_shadow=0x7f040002;
+        public static final int text_shadow_white=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,25 +28,99 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int add=0x7f020000;
+        public static final int blackgirl=0x7f020001;
+        public static final int blueboy=0x7f020002;
+        public static final int calendar=0x7f020003;
+        public static final int cancel=0x7f020004;
+        public static final int compassblue=0x7f020005;
+        public static final int compasspink=0x7f020006;
+        public static final int dividerlarge=0x7f020007;
+        public static final int editblue=0x7f020008;
+        public static final int editpink=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int pinkgirl=0x7f02000b;
+        public static final int save=0x7f02000c;
+        public static final int searchicon=0x7f02000d;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int RelativeLayout1=0x7f0a0000;
+        public static final int action_settings=0x7f0a0021;
+        public static final int button1=0x7f0a0003;
+        public static final int button2=0x7f0a0014;
+        public static final int dividerlarge=0x7f0a000a;
+        public static final int editText1=0x7f0a0008;
+        public static final int editText2=0x7f0a0006;
+        public static final int editText3=0x7f0a0005;
+        public static final int editText4=0x7f0a0004;
+        public static final int framelayout1=0x7f0a0011;
+        public static final int imageView1=0x7f0a0001;
+        public static final int imageView2=0x7f0a000b;
+        public static final int imageView3=0x7f0a000c;
+        public static final int linear1=0x7f0a000f;
+        public static final int listView1=0x7f0a001a;
+        public static final int radioButton1=0x7f0a0018;
+        public static final int radioButton2=0x7f0a0019;
+        public static final int radioG=0x7f0a0012;
+        public static final int relative1=0x7f0a0010;
+        public static final int spinner1=0x7f0a0015;
+        public static final int spinner2=0x7f0a0016;
+        public static final int spinner3=0x7f0a0017;
+        public static final int textView1=0x7f0a0002;
+        public static final int textView10=0x7f0a001e;
+        public static final int textView11=0x7f0a001f;
+        public static final int textView2=0x7f0a0009;
+        public static final int textView3=0x7f0a0007;
+        public static final int textView4=0x7f0a000e;
+        public static final int textView5=0x7f0a000d;
+        public static final int textView6=0x7f0a0013;
+        public static final int textView7=0x7f0a001b;
+        public static final int textView8=0x7f0a001c;
+        public static final int textView9=0x7f0a001d;
+        public static final int title=0x7f0a0020;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int detailboy_screen=0x7f030000;
+        public static final int detailgirl_screen=0x7f030001;
+        public static final int placeholderpart_screen=0x7f030002;
+        public static final int profile_screen=0x7f030003;
+        public static final int profileplaceholder_screen=0x7f030004;
+        public static final int startup_screen=0x7f030005;
+        public static final int titleview=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int addkiddo=0x7f06000d;
+        public static final int app_name=0x7f060000;
+        public static final int birthday=0x7f060015;
+        public static final int bname=0x7f060014;
+        public static final int bplace=0x7f060018;
+        public static final int cancel=0x7f060013;
+        public static final int dob=0x7f06000f;
+        public static final int eighth=0x7f06000a;
+        public static final int eleventh=0x7f06000c;
+        public static final int fifth=0x7f060006;
+        public static final int first=0x7f060003;
+        public static final int fourth=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int imp=0x7f060010;
+        public static final int length=0x7f060016;
+        public static final int name=0x7f06000e;
+        public static final int ninth=0x7f060008;
+        public static final int or=0x7f060011;
+        public static final int save=0x7f060012;
+        public static final int seventh=0x7f060009;
+        public static final int sixth=0x7f060007;
+        public static final int tenth=0x7f06000b;
+        public static final int third=0x7f060004;
+        public static final int weight=0x7f060017;
     }
     public static final class style {
         /** 
@@ -59,10 +144,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
